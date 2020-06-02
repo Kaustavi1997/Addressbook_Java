2f1bd15a6e68ccaf5d67eb78d5877a78e289d85f
“Kaustavi <kaustavisen8@gmail.com>
2020-06-03 01:45:25 +0530

    Interface of AddressBookApplication

---
+++ AddressbookInterface2.java
@@ -0,0 +1,7 @@
+public interface AddressbookInterface2{
+	public void create();
+	public void saveAs(String f);
+	public void diplayOptions();
+	public void toggle();
+	public void savefinal(String f,int curr_ab_idx);
+}
